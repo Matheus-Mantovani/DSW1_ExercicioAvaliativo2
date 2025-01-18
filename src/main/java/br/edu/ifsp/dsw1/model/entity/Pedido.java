14efa8c1bfa649ef79a904cf7021047c105cd19b
matheus mantovani <mantovanimatheus335@gmail.com>
2025-01-18 19:19:39 -0300

    entidades usuario e pedidos

--- src/main/java/br/edu/ifsp/dsw1/model/entity/Pedido.java
+++ src/main/java/br/edu/ifsp/dsw1/model/entity/Pedido.java
@@ -1,5 +1,64 @@
 package br.edu.ifsp.dsw1.model.entity;
 
 public class Pedido {
+	private int idPedido;
+	private String nomeCliente;
+	private String enderecoEntrega;
+	private double valor;
+	private String descricao;
+	
 
+	public Pedido() {
+		super();
+	}
+
+	public Pedido(int idPedido, String nomeCliente, String enderecoEntrega, double valor, String descricao) {
+		super();
+		this.idPedido = idPedido;
+		this.nomeCliente = nomeCliente;
+		this.enderecoEntrega = enderecoEntrega;
+		this.valor = valor;
+		this.descricao = descricao;
+	}
+
+	
+	public int getIdPedido() {
+		return idPedido;
+	}
+
+	public void setIdPedido(int idPedido) {
+		this.idPedido = idPedido;
+	}
+
+	public String getNomeCliente() {
+		return nomeCliente;
+	}
+
+	public void setNomeCliente(String nomeCliente) {
+		this.nomeCliente = nomeCliente;
+	}
+
+	public String getEnderecoEntrega() {
+		return enderecoEntrega;
+	}
+
+	public void setEnderecoEntrega(String enderecoEntrega) {
+		this.enderecoEntrega = enderecoEntrega;
+	}
+
+	public double getValor() {
+		return valor;
+	}
+
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
 }
--- src/main/java/br/edu/ifsp/dsw1/model/entity/Usuario.java
+++ src/main/java/br/edu/ifsp/dsw1/model/entity/Usuario.java
@@ -1,5 +1,34 @@
 package br.edu.ifsp.dsw1.model.entity;
 
 public class Usuario {
+	private String login;
+	private String senha;
+	
+	
+	public Usuario() {
+		super();
+	}
 
+	public Usuario(String login, String senha) {
+		super();
+		this.login = login;
+		this.senha = senha;
+	}
+
+	
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
 }
